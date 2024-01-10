367110c715d4d72caf5db2438269e5a690528df1
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-01-10 18:15:56 +0100

    implementation of BaseModel from dictionary and the test case

--- models/base_model.py
+++ models/base_model.py
@@ -4,26 +4,39 @@
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-    
-    def __str__(self) :
-         """Returns a string representation of the instance"""
-         class_name = self.__class__.__name__
-         return f"[{class_name}] ({self.id}) {self.__dict__}"
-    
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of the BaseModel class."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        setattr(
+                            self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                        )
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
+
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-         """Convert instance into dict format"""
-         result_dict = self.__dict__.copy()
-         result_dict['created_at'] = self.created_at.isoformat()
-         result_dict['updated_at'] = self.updated_at.isoformat()
-         result_dict['__class__'] = self.__class__.__name__
-         return result_dict
+        """Convert instance into dict format"""
+        result_dict = self.__dict__.copy()
+        result_dict["created_at"] = self.created_at.isoformat()
+        result_dict["updated_at"] = self.updated_at.isoformat()
+        result_dict["__class__"] = self.__class__.__name__
+        return result_dict
