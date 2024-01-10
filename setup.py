e621e348e89f11059dbf3d50f2217ac8c406a591
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-01-10 17:42:05 +0100

    created the readme and other files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AirBnB_clone_demo',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies the project may have
+    ],
+)
