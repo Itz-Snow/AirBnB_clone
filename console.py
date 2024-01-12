5a516218e27e86424dca698651283551e7427c7d
InemesitMatthew <inemesit.e.matthew@gmail.com>
2024-01-12 20:10:41 +0100

    Console 1.0 Implementation (it has been completely tested by inemesitmatthew )

--- console.py
+++ console.py
@@ -6,6 +6,12 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User  # Import the User class
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -129,6 +135,7 @@ class HBNBCommand(cmd.Cmd):
 
         setattr(instance, attribute_name, attribute_value)
         instance.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,16 +2,23 @@
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """methods returns all dictionary objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id."""
         key = f"{type(obj).__name__}.{obj.id}"
@@ -23,19 +30,19 @@ class FileStorage:
         for key, value in self.__objects.items():
             serialized_objects[key] = value.to_dict()
 
-        with open(self.__file_path, 'w') as file:
+        with open(self.__file_path, "w") as file:
             json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(self.__file_path, "r") as file:
                 loaded_objects = json.load(file)
 
             for key, value in loaded_objects.items():
-                class_name = value['__class__']
-                del value['__class__']
-                
+                class_name = value["__class__"]
+                del value["__class__"]
+
                 # Use globals() to access the global scope
                 if class_name in globals():
                     obj = globals()[class_name](**value)
@@ -45,4 +52,5 @@ class FileStorage:
             pass
 
 
-storage = FileStorage()  # Move this line outside the class definition
+# Move this line outside the class definition
+storage = FileStorage()
